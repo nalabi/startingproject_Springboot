04ce94c0e0fd1bd1b9f9fe3ecc72d4f921521b21
nalabi <bongimusa.nala@gmail.com>
2024-05-08 18:04:48 +0200

    Added Life Cycle Methods

--- src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,14 +1,16 @@
 package com.luv2code.junitdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
+    @BeforeEach
     @Test
     void testEqualsAndNotEquals() {
         DemoUtils demoUtils = new DemoUtils();
-        assertEquals(6, demoUtils.add(2, 6), "2+4 must be 6");
+        assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 9");
     }
 
