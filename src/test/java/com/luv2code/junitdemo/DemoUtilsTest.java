3fee46fe3c8770fcc31b55b52b15b7c72681f732
nalabi <bongimusa.nala@gmail.com>
2024-05-09 06:14:43 +0200

    Added Life Cycle Methods

--- src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,22 +1,45 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
+
+    DemoUtils demoUtils;
     @BeforeEach
+void setupBeforeEach(){
+        demoUtils = new DemoUtils();
+        System.out.println("Setup Before Each execution");
+    }
+@AfterEach
+    void tearDownAfterEAch() {
+    System.out.println("Tear Down After Each execution");
+    System.out.println();
+}
+@BeforeAll
+        static void setupBeforeEachClass(){
+    System.out.println("Setup Before All execution");
+    }
+
+
+    @AfterAll
+    static void tearDownAfterEachClass(){
+        System.out.println("Tear Down After All execution");
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testEqualsAndNotEquals");
+
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 9");
     }
 
     @Test
     void testNullAndNotNull(){
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testNullAndNotNull");
+
 
         String str1 = null;
         String str2 = "luv2code";
